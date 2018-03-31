e786e0fad9b115e35afe7796ce296c6a9bcc7359
161250132 <161250132@smail.nju.edu.cn>
2018-03-31 22:01:10 +0800

    更新逻辑层： 用户登陆模块接口 众包工人模块接口

--- src/main/java/com/example/maven/controller/template/SignInController.java
+++ src/main/java/com/example/maven/controller/template/SignInController.java
@@ -1,4 +1,35 @@
 package com.example.maven.controller.template;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SignInController {
+
+    /*
+    判断是否重名
+    */
+    @RequestMapping(value = "/SignInController/isExist", method = RequestMethod.GET)
+    public boolean isExist(String userId){
+        return false;
+    }
+
+    /*
+    判断是否注册成功
+    */
+    @RequestMapping(value = "/SignInController/register", method = RequestMethod.GET)
+    public boolean register(String userId){
+        return false;
+    }
+
+    /*
+    判断是否登陆成功
+    */
+    @RequestMapping(value = "/SignInController/signIn", method = RequestMethod.GET)
+    public boolean signIn(String userId, String password){
+        return false;
+    }
+
+
 }
--- src/main/java/com/example/maven/controller/template/WorkerController.java
+++ src/main/java/com/example/maven/controller/template/WorkerController.java
@@ -1,4 +1,68 @@
 package com.example.maven.controller.template;
 
+import com.example.maven.model.Task;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class WorkerController {
+
+    /*
+    获取所有未完成的任务
+     */
+    @RequestMapping(value = "/WorkerController/getAllUnfinishedTasks", method = RequestMethod.GET)
+    public List<Task> getAllUnfinishedTasks(String userId){
+        return null;
+    }
+
+    /*
+    获取所有已完成的任务
+    */
+    @RequestMapping(value = "/WorkerController/getAllFinishedTasks", method = RequestMethod.GET)
+    public List<Task> getAllFinishedTasks(String userId){
+        return null;
+    }
+
+    /*
+    根据任务Id获取任务
+     */
+    @RequestMapping(value = "/WorkerController/getTaskById", method = RequestMethod.GET)
+    public Task getTaskById(String taskId){
+        return null;
+    }
+
+    /*
+    保存一次标注
+     */
+    @RequestMapping(value = "/WorkerController/saveLabel", method = RequestMethod.GET)
+    public boolean saveLabel(){
+        return false;
+    }
+
+    /*
+    提交一次完成的任务
+     */
+    @RequestMapping(value = "/WorkerController/submitFinishedTask", method = RequestMethod.GET)
+    public boolean submitFinishedTask(){
+        return false;
+    }
+
+    /*
+    获取用户的积分
+     */
+    @RequestMapping(value = "/WorkerController/getUserScore", method = RequestMethod.GET)
+    public int getUserScore(){
+        return 0;
+    }
+
+    /*
+    获取用户的排名
+     */
+    @RequestMapping(value = "/WorkerController/getUserRanking", method = RequestMethod.GET)
+    public int getUserRanking(){
+        return 0;
+    }
 }
