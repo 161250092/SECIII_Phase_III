0ec31f9604b63379a24ddd13fcfa445aa35856e8
161250132 <161250132@smail.nju.edu.cn>
2018-03-31 21:46:56 +0800

    更新类

---
+++ src/main/java/com/example/maven/model/Task.java
@@ -0,0 +1,11 @@
+package com.example.maven.model;
+
+public class Task {
+    String taskId;
+    String labelType;
+    String[] imageName;
+    String description;
+    int requiredNumber;
+    int finishedNumber;
+    int score;
+}
