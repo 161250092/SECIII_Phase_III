77b6e662e2c30bbfc7609194825724bbb7132a30
161250054 <161250054@smail.nju.edu.cn>
2018-04-05 00:32:04 +0800

    登陆界面

---
+++ src/main/resources/static/js/login/login.js
@@ -0,0 +1,17 @@
+$("#loginButton").click(function (ev) {
+    var userId = document.getElementById("userId").value;
+    var password = document.getElementById("password").value;
+    // var loginForm = new FormData(document.getElementById("loginForm"));
+    $.ajax({
+        type: "POST",
+        url: "/LoginController/login",
+        data: {
+            userId: userId,
+            password: password
+        },
+        // data: loginForm,
+        success:function (result) {
+            alert(result);
+        }
+    });
+});
