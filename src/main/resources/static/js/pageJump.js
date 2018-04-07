b2c083498679389e2dd4e7a22a9aca1f697982dc
161250054 <161250054@smail.nju.edu.cn>
2018-04-07 21:59:52 +0800

    页面跳转

--- src/main/resources/static/js/pageJump.js
+++ src/main/resources/static/js/pageJump.js
@@ -15,6 +15,28 @@ function jumpToAnotherPage(url,userId){
 }
 
 
+//各页面URL
+var mainPageUrl = "MainPage.html";
+var loginPageUrl = "login.html";
+var registerPageUrl = "register.html";
+
+var doTaskPageUrl = "doTask.html";
+var getTaskPageUrl = "getTask.html";
+var workerCheckTaskPageUrl = "WorkerCheckTask.html";
+
+var checkMyTaskPageUrl = "CheckMyTask.html";
+var uploadTaskPageUrl = "UploadTask.html";
+
+var lineMarkPageUrl = "LineMarkPage.html";
+var markFrameLabelPageUrl = "markFrameLabel.html";
+var overAllPageUrl = "OverAllPage.html";
+
+var checkFrameLabelPageUrl = "CheckFrameLabel.html";
+var checkLineLabelPageUrl = "CheckLineLabel.html";
+var checkOverAllLabelPageUrl = "CheckOverAllLabel.html";
+
+
+
 
 
 
