1ae206820f1c5fc31c8294327e67343bc2397003
161250054 <161250054@smail.nju.edu.cn>
2018-04-10 14:31:04 +0800

    修改jquery文件路径

--- src/main/resources/static/js/admin/admin.js
+++ src/main/resources/static/js/admin/admin.js
@@ -1,21 +1,25 @@
-function WebsiteStatistics(numOfUsers, numOfTasks, numOfIncompleteTasks, numOfAccomplishedTask){
-    this.numOfUsers = numOfUsers;
-    this.numOfTasks = numOfTasks;
-    this.numOfIncompleteTasks = numOfIncompleteTasks;
-    this.numOfAccomplishedTask = numOfAccomplishedTask;
-}
-
-window.onload = function (ev) {
-    $.ajax({
-        type: "GET",
-        url: "/AdministerController/uploadImages",
-
-        success:function (result) {
-            var websiteStatistics = JSON.parse(result);
-            $("#numOfUsers").html(websiteStatistics.numOfUsers);
-            $("#numOfTasks").html(websiteStatistics.numOfTasks);
-            $("#numOfIncompleteTasks").html(websiteStatistics.numOfIncompleteTasks);
-            $("#numOfAccomplishedTask").html(websiteStatistics.numOfAccomplishedTask);
+var vm = new Vue({
+    el:"#statisticsTable",
+    data:{
+        numOfUsers: 0,
+        numOfTasks: 0,
+        numOfIncompleteTasks: 0,
+        numOfAccomplishedTask: 0
+    },
+    mounted:function (ev) {
+        this.$nextTick(function () {
+            this.getWebsiteStatistics();
+        })
+    },
+    methods:{
+        getWebsiteStatistics:function () {
+            var _this = this;
+            axios.get("/AdministerController/uploadImages").then(function (value) {
+                _this.numOfUsers = value.data.numOfUsers;
+                _this.numOfTasks = value.data.numOfTasks;
+                _this.numOfIncompleteTasks = value.data.numOfIncompleteTasks;
+                _this.numOfAccomplishedTask = value.data.numOfAccomplishedTask;
+            })
         }
-    });
-}
+    }
+});
