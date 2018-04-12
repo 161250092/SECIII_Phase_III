70aa48818fffee1a868df99dc7683edc30e79be4
161250132 <161250132@smail.nju.edu.cn>
2018-04-12 14:20:45 +0800

    更新部分接口

--- src/main/java/com/example/maven/businessLogic/markLabel/imageBL/ImageBLService.java
+++ src/main/java/com/example/maven/businessLogic/markLabel/imageBL/ImageBLService.java
@@ -1,7 +1,7 @@
 package com.example.maven.businessLogic.markLabel.imageBL;
 
 /**
- * 为所有标注提供统一的接口
+ * 获取任务中包含的图片信息
  */
 
 public interface ImageBLService {
@@ -22,24 +22,5 @@ public interface ImageBLService {
      * @return 所需图片的标注结果(JSON)
      */
     String getImageAndLabelnfo(int imageIndex, String taskId, String userId);
-
-    /**
-     * 保存一次标注的结果
-     * 根据传入的参数决定标注类型
-     * @param taskId 任务
-     * @param userId 用户ID
-     * @param type　标注类型
-     * @param labelVOJson 标注的内容
-     * @return 是否保存成功
-     */
-    boolean saveLabel(String taskId, String userId, String type, String labelVOJson);
-
-    /**
-     * 提交一次完成的任务
-     * @param userId
-     * @param taskId 任务
-     * @return 是否提交成功
-     */
-    boolean submitAccomplishedTask(String userId, String taskId);
 
 }
--- src/main/java/com/example/maven/businessLogic/workerBL/WorkerBLService.java
+++ src/main/java/com/example/maven/businessLogic/workerBL/WorkerBLService.java
@@ -6,11 +6,38 @@ import com.example.maven.model.po.Task;
 import java.util.List;
 
 public interface WorkerBLService {
+    /**
+     * 获取所有接受且已完成的任务
+     * @param userId 用户Id
+     * @return 所有已完成的任务
+     */
     List<Task> getAcceptedAndAccomplishedTaskList(String userId);
+
+    /**
+     * 获取所有接受但未完成的任务
+     * @param userId 用户Id
+     * @return 所有未完成的任务
+     */
     List<Task> getAcceptedButIncompleteTaskList(String userId);
+
+    /**
+     * 根据任务Id获取任务
+     * @param taskId 任务Id
+     * @return 任务详情
+     */
     Task getTaskById(String taskId);
-    boolean saveLabel(Label label);
-    boolean submitAccomplishedTask(Task task);
+
+    /**
+     * 获取用户的积分
+     * @param userId 用户编号
+     * @return 用户积分
+     */
     int getUserScore(String userId);
+
+    /**
+     * 获取用户的排名
+     * @param userId 用户Id
+     * @return 用户排名
+     */
     int getUserRanking(String userId);
 }
