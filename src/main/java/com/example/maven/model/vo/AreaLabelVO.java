f9c7250303ae2647eb35c683b871dc516789b36f
161250132 <161250132@smail.nju.edu.cn>
2018-04-12 15:20:38 +0800

    更新部分实体类

--- src/main/java/com/example/maven/model/vo/AreaLabelVO.java
+++ src/main/java/com/example/maven/model/vo/AreaLabelVO.java
@@ -3,13 +3,12 @@ package com.example.maven.model.vo;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class AreaLabelVO extends LabelVO implements Serializable{
+public class AreaLabelVO implements Serializable{
     private String image;
     private String label;
     private ArrayList<String> lineList;
 
     public AreaLabelVO(String image, String label, ArrayList<String> lineList) {
-        super("AreaLabel");
         this.image = image;
         this.label = label;
         this.lineList = lineList;
--- src/main/java/com/example/maven/model/vo/ImageLabelVO.java
+++ src/main/java/com/example/maven/model/vo/ImageLabelVO.java
@@ -2,12 +2,11 @@ package com.example.maven.model.vo;
 
 import java.io.Serializable;
 
-public class ImageLabelVO extends LabelVO implements Serializable{
+public class ImageLabelVO implements Serializable{
     private String image;
     private String label;
 
     public ImageLabelVO(String image, String label) {
-        super("ImageLabel");
         this.image = image;
         this.label = label;
     }
