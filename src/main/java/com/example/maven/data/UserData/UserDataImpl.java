2fb3b19feb224f5873d06775869c890cb3966fa8
161250093 <18851822790@sina.cn>
2018-04-27 20:45:46 +0800

    数据层

--- src/main/java/com/example/maven/data/UserData/UserDataImpl.java
+++ src/main/java/com/example/maven/data/UserData/UserDataImpl.java
@@ -95,7 +95,7 @@ public class UserDataImpl implements UserDataService{
             String useR = fh.readFile(userInformation);
             String[] usER = useR.split(",");
 
-            User user = new User(usER[0],usER[2],usER[1],publishedTask,acceptedTask,Integer.parseInt(usER[3]),Boolean.getBoolean(usER[4]));
+            User user = new User(usER[0],usER[2],usER[1],publishedTask,acceptedTask,Integer.parseInt(usER[3]),Boolean.parseBoolean(usER[4]));
 
             User.add(user);
         }
