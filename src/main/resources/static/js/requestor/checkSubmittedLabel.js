8b343c0221c2dfa78a92ae9f361fed72da926199
161250054 <161250054@smail.nju.edu.cn>
2018-05-23 17:04:04 +0800

    修改发布者查看任务界面

--- src/main/resources/static/js/requestor/checkSubmittedLabel.js
+++ src/main/resources/static/js/requestor/checkSubmittedLabel.js
@@ -11,5 +11,36 @@ new Vue({
                 _this.submittedLabelListOfAllTask = response.data.list;
             });
         });
-    }
+    },
+    methods: {
+        checkThisLabel: function (workerIndex) {
+            alert(workerIndex)
+        },
+        passThisLabel: function (workerIndex) {
+            alert(workerIndex)
+        },
+        rejectThisLabel: function (workerIndex) {
+            alert(workerIndex)
+        },
+        abandonThisLabel: function (workerIndex) {
+            alert(workerIndex)
+        },
+        setSelectedTaskIndex: function (value) {
+            if(this.selectedTask === value){
+                this.selectedTask = -1;
+            }else{
+                this.selectedTask = value;
+            }
+        },
+        chineseLabelTypeName: function (labelType) {
+            switch (labelType){
+                case "ImageLabel": return "整体标注";
+                case "FrameLabel": return "标框标注";
+                case "AreaLabel": return "区域标注";
+                default:
+                    alert("标注类型错误");
+                    break;
+            }
+        }
+    },
 });
