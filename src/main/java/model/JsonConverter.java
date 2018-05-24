9e44af7a1320d0768203f86140406027065733ec
161250054 <161250054@smail.nju.edu.cn>
2018-05-24 20:38:56 +0800

    修改Json转换器

--- src/main/java/model/JsonConverter.java
+++ src/main/java/model/JsonConverter.java
@@ -7,11 +7,11 @@ import java.lang.reflect.Type;
 public class JsonConverter {
     private static Gson gson = new GsonBuilder().create();
 
-    public static String toJson(Object object){
+    public static String objectToJson(Object object){
         return gson.toJson(object);
     }
 
-    public static Object toObject(String jsonStr, Type objectType){
+    public static Object jsonToObject(String jsonStr, Type objectType){
         return gson.fromJson(jsonStr, objectType);
     }
 }
