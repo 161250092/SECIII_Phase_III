2c2023ba28a6dacda79fbc5906bbe522b3afb964
161250054 <161250054@smail.nju.edu.cn>
2018-05-24 20:18:27 +0800

    修改

--- src/main/java/controller/WorkerController.java
+++ src/main/java/controller/WorkerController.java
@@ -4,6 +4,9 @@ import businessLogic.workerBL.WorkerBLImpl;
 import businessLogic.workerBL.WorkerBLService;
 import model.primitiveType.TaskId;
 import model.primitiveType.UserId;
+import model.task.AcceptedTask;
+import model.vo.AcceptedTaskVO;
+import model.vo.PublishedTaskVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +17,7 @@ import java.util.List;
  * 众包工人控制器
  */
 @RestController
-public class WorkerController {
+public class WorkerController implements WorkerBLService{
 
     private WorkerBLService workerBL;
 
@@ -57,4 +60,38 @@ public class WorkerController {
         return workerBL.getUserRanking(new UserId(userId));
     }
 
+    @Override
+    public List<AcceptedTaskVO> getAcceptedAndAccomplishedTaskList(UserId userId) {
+        return null;
+    }
+
+    @Override
+    public List<AcceptedTaskVO> getAcceptedButIncompleteTaskList(UserId userId) {
+        return null;
+    }
+
+    @Override
+    public List<PublishedTaskVO> getAvailableTaskList(UserId userId) {
+        return null;
+    }
+
+    @Override
+    public Exception acceptTask(UserId userId, TaskId[] taskIdList) {
+        return null;
+    }
+
+    @Override
+    public Exception abandonTaskByWorker(UserId userId, TaskId taskId) {
+        return null;
+    }
+
+    @Override
+    public AcceptedTask getAcceptedTaskById(TaskId taskId) {
+        return null;
+    }
+
+    @Override
+    public int getUserRanking(UserId userId) {
+        return 0;
+    }
 }
