31082f7955134a1b705aa4396b19a7c2dd90469b
161250054 <161250054@smail.nju.edu.cn>
2018-05-24 20:11:49 +0800

    修改接受任务状态类

--- src/main/java/model/task/AcceptedTaskState.java
+++ src/main/java/model/task/AcceptedTaskState.java
@@ -2,6 +2,6 @@ package model.task;
 
 
 public enum AcceptedTaskState {
-    //接受，完成，通过，驳回，废弃
-    ACCEPTED, SUBMITTED, PASSED, REJECTED, ABANDONED
+    //接受，完成，通过，驳回，被发布者废弃，被工人自己废弃
+    ACCEPTED, SUBMITTED, PASSED, REJECTED, ABANDONED_BY_REQUESTOR, ABANDONED_BY_WORKER;
 }
