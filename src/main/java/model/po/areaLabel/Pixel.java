2e177944eead4a68ad573b87b375be6b556b0d04
161250054 <161250054@smail.nju.edu.cn>
2018-05-24 11:46:07 +0800

    修改区域标注的类

---
+++ src/main/java/model/po/areaLabel/Pixel.java
@@ -0,0 +1,14 @@
+package model.po.areaLabel;
+
+/**
+ * 区域标注中线的像素点
+ */
+public class Pixel {
+    public double x;
+    public double y;
+
+    public Pixel(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
