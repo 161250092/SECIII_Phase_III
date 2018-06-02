558b78b41ac63bb8e7784229191b3666c7012c5e
161250132 <161250132@smail.nju.edu.cn>
2018-06-02 16:14:34 +0800

    更新controller

--- src/main/java/maven/controller/RegisterController.java
+++ src/main/java/maven/controller/RegisterController.java
@@ -3,7 +3,9 @@ package maven.controller;
 import maven.businessLogic.registerBL.RegisterBLImpl;
 import maven.businessLogic.registerBL.RegisterBLService;
 import maven.businessLogic.registerBL.RegisterBLStub;
+import maven.model.primitiveType.Email;
 import maven.model.primitiveType.Password;
+import maven.model.primitiveType.Phone;
 import maven.model.primitiveType.Username;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,8 +37,8 @@ public class RegisterController {
      * @return 是否注册成功
      */
     @RequestMapping(value = "/register/register", method = RequestMethod.GET)
-    public Exception register(String username, String password){
-        return registerBL.register(new Username(username), new Password(password));
+    public Exception register(String username, String password, String email, String phone){
+        return registerBL.register(new Username(username), new Password(password), new Email(email), new Phone(phone));
     }
 
 }
