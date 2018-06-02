02621ffdce1b84d7449dc719763ae406b38d61ff
Examinee <examinee@smail.nju.edu.cn>
2018-06-02 21:57:25 +0800

    JS

--- src/main/resources/static/js/requestor/unfinishedTasksPage.js
+++ src/main/resources/static/js/requestor/unfinishedTasksPage.js
@@ -1,11 +1,11 @@
 new Vue({
     el:"#taskInfoContainer",
     data:{
-        userId: "",
+
         AllUnfinishedTasks:[],
         taskDescription:"请点击要查看的任务",
         reviseTaskId:"无",
-        reviseTaskPrice:"0",
+        reviseTaskPrice:0,
 
         IMAGE_LABEL_TYPE: "ImageLabel",
         FRAME_LABEL_TYPE: "FrameLabel",
@@ -16,12 +16,13 @@ new Vue({
         this.$nextTick(function () {
             _this.userId = getUserId();
             axios.get("/requestor/getAssignedButIncompleteTaskList",
-                { params:{ userId: this.userId } })
+                { params:{ userId: getUserId() } })
                 .then(function (response) {
                     _this.AllUnfinishedTasks = response.data;
                 })
         })
     },
+
     methods:{
         getChineseLabelType: function (labelType) {
             if(labelType === this.IMAGE_LABEL_TYPE){
@@ -34,11 +35,11 @@ new Vue({
         },
 
         detailedInfo:function(index){
-            this.taskDescription = this.AllFinishedTasks[index].taskDescription;
+            this.taskDescription = this.AllUnfinishedTasks[index].taskDescription;
         },
 
         changeTaskPrice:function(index){
-            this.reviseTaskId = this.AllFinishedTasks[index].taskId;
+            this.reviseTaskId = this.AllUnfinishedTasks[index].taskId;
         },
 
 
@@ -47,23 +48,33 @@ new Vue({
 
             axios.get("/requestor/revokeTask", {params: {taskId:this.AllUnfinishedTasks[index].taskId}})
                 .then(function (Exception) {
-                    let message = Exception.data.WrongMessage.type;
-                    if(message === "Success")
+                    //let message = Exception.data.WrongMessage.type;
+                    if(message==="Success")
                         alert("撤销成功");
                     else
                         alert("撤销失败")
+
                 });
         },
 
         addTaskPrice:function(){
+
             axios.get("/requestor/reviseTask", {params: {taskId:this.reviseTaskId,cash:this.reviseTaskPrice}})
                 .then(function (Exception) {
-                    let message = Exception.data.WrongMessage.type;
-                    if(message === "Success")
+                    //let message = Exception.data.WrongMessage.type;
+                    if(message==="Success")
                         alert("修改成功");
                     else
                         alert("修改失败")
+
                 });
         }
+
     }
+
+
+
+
+
+
 });
