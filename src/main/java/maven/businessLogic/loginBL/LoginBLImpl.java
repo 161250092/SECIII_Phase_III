821bebe68f6374428547a2f95208a28c7eba7276
161250132 <161250132@smail.nju.edu.cn>
2018-06-03 17:42:12 +0800

    更新BL

--- src/main/java/maven/businessLogic/loginBL/LoginBLImpl.java
+++ src/main/java/maven/businessLogic/loginBL/LoginBLImpl.java
@@ -1,19 +1,56 @@
 package maven.businessLogic.loginBL;
 
-//import data.UserData.UserDataImpl;
-//import data.UserData.UserDataService;
+import maven.data.AdminData.AdminDataImpl;
+import maven.data.AdminData.AdminDataService;
+import maven.data.UserData.UserDataImpl;
+import maven.data.UserData.UserDataService;
+import maven.exception.LoginException.*;
 import maven.model.primitiveType.Password;
+import maven.model.primitiveType.UserId;
 import maven.model.primitiveType.Username;
+import maven.model.user.Admin;
+import maven.model.user.Requestor;
+import maven.model.user.User;
+import maven.model.user.Worker;
+
+import java.util.List;
 
 public class LoginBLImpl implements LoginBLService {
-    //private UserDataService userDataService;
+    private UserDataService userDataService;
+    private AdminDataService adminDataService;
 
     public LoginBLImpl(){
-        //userDataService = new UserDataImpl();
+        userDataService = new UserDataImpl();
+        adminDataService = new AdminDataImpl();
     }
 
     @Override
     public Exception login(Username username, Password password) {
-        return null;
+        List<Username> usernameList = userDataService.getAllUsernameList();
+        for(Username name : usernameList){
+            if(username.value.equals(name.value)){
+                UserId userId = userDataService.getUserId(name);
+                User user = userDataService.getUserByUserId(userId);
+                if(password.value.equals(user.getPassword().value)){
+                    if(user instanceof Worker)
+                        return new WorkerLoginException(userId);
+                    else if(user instanceof Requestor)
+                        return new RequestorLoginException(userId);
+                }
+                else
+                    return new LoginErrorException();
+            }
+        }
+
+        List<Admin> adminList = adminDataService.getAllAdmin();
+        for(Admin admin : adminList){
+            if(username.value.equals(admin.getUsername().value)){
+                if(password.value.equals(admin.getPassword().value))
+                    return new AdministerLoginException();
+                else
+                    return new LoginErrorException();
+            }
+        }
+        return new UsernameNotExistsException();
     }
 }
