dc0015114e728b4e574ac3a6267c08ef6f765b32
161250093 <18851822790@sina.cn>
2018-06-09 15:33:01 +0800

    数据层

--- src/main/java/maven/data/LabelQualityData/LabelQualityDataImpl.java
+++ src/main/java/maven/data/LabelQualityData/LabelQualityDataImpl.java
@@ -1,12 +1,57 @@
 package maven.data.LabelQualityData;
 
+import maven.data.MySQL.MySQLConnector;
+import maven.model.primitiveType.UserId;
 import maven.model.task.AcceptedTaskQuality;
+import maven.model.task.AcceptedTaskState;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LabelQualityDataImpl implements LabelQualityDataService{
     @Override
     public List<AcceptedTaskQuality> getAllAcceptedTaskQualityList() {
-        return null;
+        Connection conn = new MySQLConnector().getConnection("AcceptedTask");
+
+        List<AcceptedTaskQuality> acceptedTaskQualities = new ArrayList<>();
+
+        PreparedStatement stmt;
+        String sql;
+        ResultSet rs;
+
+        try {
+            sql = "select * from AcceptedTask where State = ? or State = ? or State = ?";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, AcceptedTaskState.PASSED.toString());
+            stmt.setString(2,AcceptedTaskState.ABANDONED_BY_REQUESTOR.toString());
+            stmt.setString(3,AcceptedTaskState.REJECTED.toString());
+
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                UserId worker = new UserId(rs.getString("UserId"));
+                String[] taskId = rs.getString("TaskId").split("_");
+                UserId requestorId = new UserId(taskId[0]);
+                AcceptedTaskState state = AcceptedTaskState.valueOf(rs.getString("State"));
+
+                AcceptedTaskQuality acceptedTaskQuality = new AcceptedTaskQuality(worker,requestorId,state);
+
+                acceptedTaskQualities.add(acceptedTaskQuality);
+            }
+
+            stmt.close();
+
+            conn.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return acceptedTaskQualities;
     }
 }
