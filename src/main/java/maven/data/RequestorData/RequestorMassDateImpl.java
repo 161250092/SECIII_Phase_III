fb0882d0711b2e1d9d88d4b6b732f07f6255c080
161250093 <18851822790@sina.cn>
2018-06-15 11:30:57 +0800

    数据层

--- src/main/java/maven/data/RequestorData/RequestorMassDateImpl.java
+++ src/main/java/maven/data/RequestorData/RequestorMassDateImpl.java
@@ -1,21 +1,130 @@
 package maven.data.RequestorData;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import maven.data.MySQL.MySQLConnector;
 import maven.model.massTask.MassTaskDetail;
+import maven.model.massTask.MassTaskPricingMechanism;
+import maven.model.primitiveType.Cash;
 import maven.model.primitiveType.TaskId;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+
 public class RequestorMassDateImpl implements RequestorMassTaskDataService {
+    private Connection conn;
+
     @Override
     public boolean saveMassTaskDetail(MassTaskDetail massTaskDetail) {
-        return false;
+        conn = new MySQLConnector().getConnection("PublishedTask");
+
+        boolean result = false;
+
+        PreparedStatement stmt;
+        String sql;
+
+        Gson gson = new GsonBuilder().create();
+        try{
+            sql = "insert into MassTaskDetail values (?,?,?,?,?,?)";
+
+            stmt = conn.prepareStatement(sql);
+
+            String sDate = gson.toJson(massTaskDetail.getStartTime());
+            String eDate = gson.toJson(massTaskDetail.getEndTime());
+
+            stmt.setString(1,massTaskDetail.getTaskId().value);
+            stmt.setDouble(2,massTaskDetail.getGivenUnitPrice().value);
+            stmt.setDouble(3,massTaskDetail.getBudget().value);
+            stmt.setString(4,massTaskDetail.getMassTaskPricingMechanism().toString());
+            stmt.setString(5,sDate);
+            stmt.setString(6,eDate);
+
+            stmt.close();
+            conn.close();
+
+            result = true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
     public MassTaskDetail getMassTaskDetail(TaskId taskId) {
-        return null;
+        conn = new MySQLConnector().getConnection("PublishedTask");
+
+        MassTaskDetail result = null;
+
+        PreparedStatement stmt;
+        String sql;
+        ResultSet rs;
+
+        Gson gson = new GsonBuilder().create();
+        try{
+            sql = "select * from MassTaskDetail where TaskId = ?";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,taskId.value);
+
+            rs = stmt.executeQuery();
+
+            while(rs.next()){
+                Cash unitPrice = new Cash(rs.getDouble("UnitPrice"));
+                Cash budget = new Cash(rs.getDouble("Budget"));
+                MassTaskPricingMechanism pricingMechanism = MassTaskPricingMechanism.valueOf(rs.getString("PricingMechanism"));
+                Date sDate = gson.fromJson(rs.getString("sDate"),Date.class);
+                Date eDate = gson.fromJson(rs.getString("eDate"),Date.class);
+
+                if(pricingMechanism.equals(MassTaskPricingMechanism.MAXIMIZE_TASKS))
+                    result = new MassTaskDetail(taskId,unitPrice,sDate,eDate);
+                else
+                    result = new MassTaskDetail(taskId,unitPrice,budget,sDate,eDate);
+
+            }
+
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
     public boolean isMassTask(TaskId taskId) {
-        return false;
+        conn = new MySQLConnector().getConnection("PublishedTask");
+
+        boolean result = false;
+
+        PreparedStatement stmt;
+        String sql;
+        ResultSet rs;
+
+        try{
+            sql = "select * from MassTaskDetail where TaskId = ?";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,taskId.value);
+
+            rs = stmt.executeQuery();
+
+            while(rs.next()){
+                result = true;
+            }
+
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
--- src/main/java/maven/data/WorkerData/WorkerMassTaskDataImpl.java
+++ src/main/java/maven/data/WorkerData/WorkerMassTaskDataImpl.java
@@ -1,18 +1,92 @@
 package maven.data.WorkerData;
 
+import maven.data.MySQL.MySQLConnector;
+import maven.model.massTask.ImageNum;
 import maven.model.massTask.WorkerBid;
+import maven.model.primitiveType.Cash;
 import maven.model.primitiveType.TaskId;
+import maven.model.primitiveType.UserId;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WorkerMassTaskDataImpl implements WorkerMassTaskDataService {
+    private Connection conn;
+
     @Override
     public boolean saveWorkerBid(WorkerBid workerBid) {
-        return false;
+        conn = new MySQLConnector().getConnection("AcceptedTask");
+
+        boolean result = false;
+
+        PreparedStatement stmt;
+        String sql;
+
+        try{
+            sql = "insert into WorkerBid values (?,?,?,?,?)";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,workerBid.getWorkerId().value);
+            stmt.setString(2,workerBid.getChosenTaskId().value);
+            stmt.setDouble(3,workerBid.getRatioOfArrivedTime());
+            stmt.setDouble(4,workerBid.getWantedUnitPrice().value);
+            stmt.setInt(5,workerBid.getMaxWantedImageNum().value);
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+
+            result = true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
     public List<WorkerBid> getAllWorkerBid(TaskId taskId) {
-        return null;
+        conn = new MySQLConnector().getConnection("AcceptedTask");
+
+        List<WorkerBid> result = new ArrayList<>();
+
+        PreparedStatement stmt;
+        String sql;
+        ResultSet rs;
+
+        try{
+            sql = "select * from WorkerBid where TaskId = ?";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,taskId.value);
+
+            rs = stmt.executeQuery();
+
+            while(rs.next()){
+                UserId userId = new UserId(rs.getString("UserId"));
+                double radio = rs.getDouble("Radio");
+                Cash cash = new Cash(rs.getDouble("Cash"));
+                ImageNum imageNum = new ImageNum(rs.getInt("ImageNum"));
+
+                WorkerBid temp = new WorkerBid(userId,taskId,radio,cash,imageNum);
+                result.add(temp);
+            }
+
+            stmt.close();
+
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return result;
     }
 }
