0bf930b27bb332565f055ba19b2864da1df8d242
161250132 <161250132@smail.nju.edu.cn>
2018-06-16 23:24:23 +0800

    更新BL

--- src/main/java/maven/businessLogic/manageUserBL/ManageUserBLImpl.java
+++ src/main/java/maven/businessLogic/manageUserBL/ManageUserBLImpl.java
@@ -1,22 +1,29 @@
 package maven.businessLogic.manageUserBL;
 
+import maven.data.Map.MapDataImpl;
+import maven.data.Map.MapDataService;
 import maven.data.UserData.UserDataImpl;
 import maven.data.UserData.UserDataService;
 import maven.model.primitiveType.Cash;
 import maven.model.primitiveType.Prestige;
+import maven.model.primitiveType.TaskNum;
 import maven.model.primitiveType.UserId;
+import maven.model.task.TaskType;
 import maven.model.user.Requestor;
 import maven.model.user.User;
 import maven.model.user.Worker;
 
 import java.util.List;
+import java.util.Map;
 
 public class ManageUserBLImpl implements ManageUserBLService {
 
     private UserDataService userDataService;
+    private MapDataService mapDataService;
 
     public ManageUserBLImpl(){
         userDataService = new UserDataImpl();
+        mapDataService = new MapDataImpl();
     }
 
     @Override
@@ -52,6 +59,24 @@ public class ManageUserBLImpl implements ManageUserBLService {
 
     @Override
     public boolean revisePrestige(UserId userId, Prestige prestige) {
+        Map<TaskType,Prestige> taskTypePrestigeMap = mapDataService.getPrestigeTaskType();
+        Prestige minPrestige = taskTypePrestigeMap.get(TaskType.ORDINARY_LEVEL_LABEL_REQUIRED);
+        Prestige maxPrestige = mapDataService.getMaxPrestige();
+
+        //将要保存到数据库内的声望
+        Prestige realPrestige = prestige;
+        //如果用户声望将要超过上限，则将用户声望置为最大值
+        if(prestige.value > maxPrestige.value)
+            realPrestige = maxPrestige;
+
+        //根据声望 修改用户的任务数量
+        double delta = prestige.value - minPrestige.value;
+        int taskNumber = (int)(delta+1);
+        TaskNum taskNum = new TaskNum(taskNumber);
+
+        userDataService.reviseTaskNum(userId, taskNum);
+
+        //修改声望
         return userDataService.revisePrestige(userId, prestige);
     }
 
@@ -60,7 +85,7 @@ public class ManageUserBLImpl implements ManageUserBLService {
         User user = userDataService.getUserByUserId(userId);
         Prestige lastPrestige = user.getPrestige();
         Prestige currentPrestige = new Prestige(lastPrestige.value - prestige.value);
-        return userDataService.revisePrestige(userId, currentPrestige);
+        return revisePrestige(userId, currentPrestige);
     }
 
     @Override
@@ -68,6 +93,6 @@ public class ManageUserBLImpl implements ManageUserBLService {
         User user = userDataService.getUserByUserId(userId);
         Prestige lastPrestige = user.getPrestige();
         Prestige currentPrestige = new Prestige(lastPrestige.value + prestige.value);
-        return userDataService.revisePrestige(userId, currentPrestige);
+        return revisePrestige(userId, currentPrestige);
     }
 }
